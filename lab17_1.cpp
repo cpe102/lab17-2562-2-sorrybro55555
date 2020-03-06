f25c9807bb5b30d857202253a681c07f035e6e9e
lab303-2 <lab303-2@labeng.local>
2020-03-06 17:22:17 +0700

    lab17_2562_2

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,1 +1,23 @@
-//Write your code here
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main()
+{
+    ifstream source;
+    source.open();
+
+    string texline;
+    while(getline()){
+        cout << << \n;
+    char format[] = _________;
+    char name[100]
+    float a,b,c;
+    sscanf(textline.c_str(),format,______)
+    cout << name << "\n";
+    cout << a << " " << b << " " << c << "\n" ;
+}
+
+
+}
